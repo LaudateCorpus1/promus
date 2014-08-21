e7c103e8c43336743b6ad64fc388e63d8e8616a9
Manuel Lopez <jmlopez.rod@gmail.com>
2014-08-21 03:35:23 -0500

    Install Command
    This will make it easier to be able to call the promus script. Similar
    as how it was done in excentury.

--- promus/command/__init__.py
+++ promus/command/__init__.py
@@ -4,7 +4,9 @@ Collection of functions to create the command line utility.
 
 """
 
+import os
 import sys
+import os.path as pth
 from dateutil import parser
 from datetime import datetime
 from subprocess import Popen, PIPE
@@ -19,6 +21,11 @@ def error(msg):
 def warn(msg):
     "Print a message to the standard error "
     sys.stderr.write(msg)
+
+
+def disp(msg):
+    "Print a message to the standard output "
+    sys.stdout.write(msg)
 
 
 def import_mod(name):
@@ -56,3 +63,18 @@ def date(short=False):
     if not short:
         return now.strftime("%a %b %d, %Y %r")
     return now.strftime("%Y-%m-%d-%H-%M-%S")
+
+
+def make_dir(path):
+    "Create a directory if it does not exist and return True. "
+    if pth.exists(path):
+        return False
+    os.makedirs(path)
+    return True
+
+
+def append_variable(var, val):
+    """Return a valid bash string to append a value to a variable. """
+    ans = 'if [[ ":$%s:" != *":%s:"* ]]; then\n' % (var, val)
+    ans += '    export %s=%s:$%s\n' % (var, val, var)
+    return ans + 'fi\n'
